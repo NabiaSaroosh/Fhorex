b75a2a8101f4113f82ec9ae7c78a66fe8f2e5f79
hamza <hamzaasmat486@gmail.com>
2017-10-22 20:07:26 +0500

    updated home page

--- Fhorex/Content/main.js
+++ Fhorex/Content/main.js
@@ -11,4 +11,10 @@
     $(".close_login_panel").click(function () {
         $(".login-panel").slideUp();
     });
+    $("#search_result").click(function () {
+        $(".search_overlay").slideDown(300);
+    });
+    $(".cancel_search").click(function () {
+        $(".search_overlay").slideUp(300);
+    });
 });
