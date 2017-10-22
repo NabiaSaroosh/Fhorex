de7ada0bdff0214d8ce1babad79589cb8020c38d
hamza <hamzaasmat486@gmail.com>
2017-10-22 15:59:19 +0500

    updated homepage

---
+++ Fhorex/Content/main.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $(".open_account_btn").click(function () {
+        $(".open_account_panel").show(200);
+    });
+    $(".close_account_panel").click(function () {
+        $(".open_account_panel").hide(200);
+    });
+    $("#login_panel").click(function () {
+        $(".login-panel").slideDown();
+    });
+    $(".close_login_panel").click(function () {
+        $(".login-panel").slideUp();
+    });
+});
